d161dbe42f69200928b282604117ae232459fefa
guale <gualeshadow@gmail.com>
2021-09-08 10:27:24 -0500

    start products page

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Products() {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col">
+					<img src="https://i.stack.imgur.com/y9DpT.jpg" />
+				</div>
+			</div>
+		</div>
+	);
+}
