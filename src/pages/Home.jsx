d7209bc33496eb3e48c22db054d30e654e70981e
luis <jmsantos879@gmail.com>
2021-09-08 17:33:45 -0400

    removed button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "../components/Cad/Cards";
 
 export default function Home() {
   return (
     <div>
-        
-
-      <h1 >Home Page</h1>
-      <Card/>
-      <div>
-        <Button variant="dark">Dark</Button>{" "}
-      </div>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center", 
+        }}
+      >
+        Super Awesome Prints
+      </h1>
+      <h4
+        style={{
+          display:"flex",
+          justifyContent:"center",
+          alignItems: "center"
+        }}
+      >
+        something something intro to our shop
+      </h4>
+      <Card /> 
     </div>
   );
 }
