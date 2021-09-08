d23d8a9d04693524fa3aa9ec2eb611cf5db289da
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-08 11:46:58 -0500

    added projects to the test page

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Products from '../components/products/Products.jsx';
 
 export default function Test() {
     return (
         <div>
             <h1>THIS PAGE IS FOR TESTING THINGS</h1>
+            <Products />
         </div>
     )
 }
