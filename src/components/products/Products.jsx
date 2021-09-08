0f873ec8942c9319e68bffe39b43c3316ee9da14
guale <gualeshadow@gmail.com>
2021-09-08 12:06:58 -0500

    finish selection menu

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./products.css"
+import "./products.css";
 
 export default class Products extends Component {
 	constructor(props) {
@@ -15,6 +15,28 @@ export default class Products extends Component {
 
 	render() {
 		let selectionElement;
+		let mediaName;
+
+		switch (this.state.media) {
+			case "gloss-photo":
+				mediaName = "Gloss Photo";
+				break;
+			case "matte-photo":
+				mediaName = "Matte Photo";
+				break;
+			case "hot-press":
+				mediaName = "Hot Press Paper";
+				break;
+			case "cold-press":
+				mediaName = "Cold Press Paper";
+				break;
+			case "alum-composite":
+				mediaName = "Aluminum Composite";
+				break;
+			case "acrylic":
+				mediaName = "Acrylic";
+				break;
+		}
 
 		switch (this.state.media) {
 			case "":
@@ -25,9 +47,12 @@ export default class Products extends Component {
 				);
 				break;
 			case "gloss-photo":
+			case "matte-photo":
+			case "hot-press":
+			case "cold-press":
 				selectionElement = (
 					<div>
-						<h3>Glossy Photo:</h3>
+						<h3>{mediaName}</h3>
 						<label for="size-select">Select Size:</label>
 						<select name="size" id="size-select">
 							<option value=""></option>
@@ -38,13 +63,51 @@ export default class Products extends Component {
 							<option value="18x24">18x24</option>
 							<option value="24x36">24x36</option>
 						</select>
-						<br/>
+						<br />
 						<label for="quantiy">Quantity:</label>
-						<input type="number" id="quantity" value="0"></input>
-						<br/>
+						<input
+							type="number"
+							id="quantity"
+							defaultValue="0"
+						></input>
+						<br />
 						<button className="btn btn-success">Add to Cart</button>
 					</div>
 				);
+				break;
+			case "alum-composite":
+			case "acrylic":
+				selectionElement = (
+					<div>
+						<h3>{mediaName}</h3>
+						<label for="size-select">Select Size:</label>
+						<select name="size" id="size-select">
+							<option value=""></option>
+							<option value="12x18">12x18</option>
+							<option value="18x24">18x24</option>
+						</select>
+						<br />
+						<label for="quantiy">Quantity:</label>
+						<input
+							type="number"
+							id="quantity"
+							defaultValue="0"
+						></input>
+						<br />
+						<p>Finishing options:</p>
+						<div>
+						<input type="radio" id="hanger" name="finishing" value="hanger"/>
+						<label for="id">Ready to hang.</label></div>
+						<div>
+						<input type="radio" id="stand-off" name="finishing" value="stand-off"/>
+						<label for="id">Stand-offs (4 corners)</label></div>
+						<div>
+						<input type="radio" id="none" name="finishing" value="none"/>
+						<label for="id">None</label></div>
+						<br/>
+						<button className="btn btn-success">Add to Cart</button>
+					</div>
+				)
 		}
 
 		return (
@@ -75,20 +138,31 @@ export default class Products extends Component {
 							<label for="media-select">Select Media:</label>
 							<select name="media" id="media-select">
 								<option value=""></option>
-								<option value="gloss-photo">
-									Glossy Photo
-								</option>
-								<option value="matte-photo">Matte Photo</option>
-								<option value="hot-press">
-									Hot Press Paper
-								</option>
-								<option value="cold-press">
-									Cold Press Paper
-								</option>
-								<option value="alum-composite">
-									Aluminum Composite
-								</option>
-								<option value="acrylic">Acrylic</option>
+								<optgroup label="Paper">
+									<option
+										value="gloss-photo"
+										label="Glossy Photo"
+									/>
+									<option
+										value="matte-photo"
+										label="Matte Photo"
+									/>
+									<option
+										value="hot-press"
+										label="Hot Press Paper"
+									/>
+									<option
+										value="cold-press"
+										label="Cold Press Paper"
+									/>
+								</optgroup>
+								<optgroup label="Rigid Substrate">
+									<option
+										value="alum-composite"
+										label="Aluminum Composite"
+									/>
+									<option value="acrylic" label="Acrylic" />
+								</optgroup>
 							</select>
 						</div>
 					</div>
