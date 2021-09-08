919ca24f76fb7f543b0fd4c858b60b63db0e665e
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-08 10:03:57 -0500

    navbar started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Super Awesome Prints</h1>
+        <Navbar />
+        
       </header>
     </div>
   );
