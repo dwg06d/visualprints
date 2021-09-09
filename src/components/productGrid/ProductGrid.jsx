7099a3706e31e7e3fc79c22d89357a9275d4cdac
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-08 23:36:13 -0500

    working on product grid component

---
+++ src/components/productGrid/ProductGrid.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {products} from '../../resources/json/ProductList.json'
+import { Link } from 'react-router-dom';
+
+export default function ProductGrid() {
+    return (
+        <div>
+            <h1>Product Grid</h1>
+            <div >
+
+          {products.map((data, key) => {
+
+            return (
+              <div key={key}>
+                  <Link to="/products/1456" className="">
+                <div className="card">
+                <img className="card-img-top" src={data.img} alt={data.title} />
+                <div className="card-body">
+                <h5 className="card-title">{data.title}</h5>
+                </div>
+                </div>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+        </div>
+    )
+}
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ProductGrid from '../components/productGrid/ProductGrid';
 
 export default function Store() {
     return (
         <div>
             <h1>Store Page</h1>
+            <ProductGrid />
+
         </div>
     )
 }
