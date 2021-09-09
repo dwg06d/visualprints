c0aaec0644ee2f67f46a8db4761c2788a4fd14c9
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-09 17:17:34 -0500

    updated links on homepage so they link to proper id

--- src/components/Cad/CardUI.jsx
+++ src/components/Cad/CardUI.jsx
@@ -1,6 +1,6 @@
 import React from "react"; 
 import { Button } from "react-bootstrap";
-import "./card-style.css"; 
+// import "./card-style.css"; 
 import { useHistory } from "react-router-dom";
 
 const Card = (props) => {
