6665072c9ee260d6646f853b6f702bbf77682127
luis <jmsantos879@gmail.com>
2021-09-09 10:02:01 -0400

    add css to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,24 +14,9 @@ export default function Home() {
   return (
     <div>
       <br/>
-      <h1
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center", 
-        }}
-      >
-        Super Awesome Prints
-      </h1>
-      <h4
-        style={{
-          display:"flex",
-          justifyContent:"center",
-          alignItems: "center"
-        }}
-      >
-        something something intro to our shop
-      </h4>
+      <h1>
+        Super Awesome Prints</h1>
+      {/* <h4>something something intro to our shop</h4> */}
       <hr/>
       <br/>
       <Card/>
