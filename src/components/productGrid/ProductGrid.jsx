37c4693edb294f16e92149595d63d464a286d60d
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-09 08:46:47 -0500

    images working after moving them

--- src/components/productGrid/ProductGrid.jsx
+++ src/components/productGrid/ProductGrid.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import {products} from '../../resources/json/ProductList.json'
 import { Link } from 'react-router-dom';
+import "./ProductGrid.css"; 
 
 export default function ProductGrid() {
     return (
         <div>
-            <h1>Product Grid</h1>
-            <div >
+            <div>
 
           {products.map((data, key) => {
 
             return (
               <div key={key}>
-                  <Link to="/products/1456" className="">
+                <Link to="/products/1456" className="">
                 <div className="card">
-                <img className="card-img-top" src={data.img} alt={data.title} />
+                    <img className="card-img-top" src={data.img} alt={data.title} />
                 <div className="card-body">
-                <h5 className="card-title">{data.title}</h5>
+                    <h5 className="card-title">{data.title}</h5>
                 </div>
                 </div>
                 </Link>
