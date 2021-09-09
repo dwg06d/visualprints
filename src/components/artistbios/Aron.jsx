9faf95c055085c4d8da489b503fa356bd95db428
guale <gualeshadow@gmail.com>
2021-09-09 10:08:12 -0500

    update bio for Aron

--- src/components/artistbios/Aron.jsx
+++ src/components/artistbios/Aron.jsx
@@ -15,10 +15,7 @@ function BiosA() {
             <div class="col-lg-5">
               <h1 class="font-weight-light">Aron</h1>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book.
+                I'm a decidedly amateur photographer who occasionally takes a few photos and even more rarely takes them off the camera to do anything with them.
               </p>
             </div>
           </div>
